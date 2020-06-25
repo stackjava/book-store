70d23f142b1008d9c3afe9a128615e7ac2ce8aff
Trần Hữu Cương <cuongth@techx.vn>
2020-06-25 03:21:42 +0000

    apply i18n Internationalization

--- src/main/java/stackjava/com/bookstore/configuration/MvcConfig.java
+++ src/main/java/stackjava/com/bookstore/configuration/MvcConfig.java
@@ -1,11 +1,19 @@
 package stackjava.com.bookstore.configuration;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.resource.VersionResourceResolver;
 
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
@@ -21,4 +29,37 @@ public class MvcConfig implements WebMvcConfigurer {
                 .resourceChain(false)
                 .addResolver(new VersionResourceResolver().addContentVersionStrategy("/**"));
     }
+
+
+    /*Bean localeResolver sẽ xác định ngôn ngữ hiển thị trên view bằng cách nào, ví dụ thông qua session, qua cookies
+        Trong ví dụ này, mình sẽ xác định ngôn ngữ hiển thị qua cookies lưu trên trình duyệt*/
+    @Bean(name = "localeResolver")
+    public LocaleResolver getLocaleResolver()  {
+        CookieLocaleResolver resolver= new CookieLocaleResolver();
+        resolver.setDefaultLocale(Locale.ENGLISH);
+        // 60 minutes
+        resolver.setCookieMaxAge(60*60);
+        return resolver;
+    }
+
+    @Bean(name = "messageSource")
+    public MessageSource getMessageResource()  {
+        ReloadableResourceBundleMessageSource messageResource= new ReloadableResourceBundleMessageSource();
+
+        // Đọc vào file i18n/messages_xxx.properties
+        // Ví dụ: i18n/messages_en.properties
+        messageResource.setBasename("classpath:i18n/messages");
+        messageResource.setDefaultEncoding("UTF-8");
+        return messageResource;
+    }
+
+    /*Method addInterceptors sẽ xác định thay đổi ngôn ngữ hiển thị thông qua param name nào trên trình duyệt.
+        Ví dụ trên URL có param: ?language=en thì tiếng anh sẽ được hiển thị*/
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
+        localeInterceptor.setParamName("language");
+        registry.addInterceptor(localeInterceptor).addPathPatterns("/*");
+    }
+
 }
